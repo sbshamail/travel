5d50cf87c034a8a61f29b1382d6040a50200bf84
hamzacodev <hamzacodev@gmail.com>
2025-05-21 13:19:30 +0500

    ok

--- src/app/navigation/useAppNavigation.tsx
+++ src/app/navigation/useAppNavigation.tsx
@@ -1,5 +1,4 @@
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import type { RootStackParamList } from './RootNavigator'; // or wherever you define it
 
-export const useAppNavigation = () =>
-  useNavigation<NavigationProp<RootStackParamList>>();
+export const useAppNavigation = () => useNavigation<NavigationProp<RootStackParamList>>();
